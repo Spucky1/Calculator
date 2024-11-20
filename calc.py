2e5a181b8e4a23d8a11375e330deb2d9858250ea
Spucky <126801350+Spucky1@users.noreply.github.com>
2024-11-19 21:04:08 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,10 +3,15 @@ from tkinter import ttk
 
 window = Tk()
 window.geometry("1920x1080")
-
+def printInput(): 
+    inp = textbox.get(1.0, "end-1c") 
+    lbl.config(text = "Provided Input: "+inp) 
 
 textbox =ttk.Entry()
 button = Button(text=("hello"))
+
 textbox.pack()
+lbl =Label(Frame, Text="")
+lbl.pack()
 button.pack()
 window.mainloop()
