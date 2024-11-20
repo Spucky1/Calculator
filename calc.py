286ab8cf36d897181d11b579c8decf9e189b7674
Spucky <126801350+Spucky1@users.noreply.github.com>
2024-11-19 20:02:59 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,10 +1,12 @@
 from tkinter import*
+from tkinter import ttk
 
 window = Tk()
 window.geometry("1920x1080")
 
 
-
+textbox =ttk.Entry()
 button = Button(text=("hello"))
+textbox.pack()
 button.pack()
 window.mainloop()
